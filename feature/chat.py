55a527a252d9f2067c2472442a843c91a1e1cdb2
Abd Wahid <wahed.blog99@gmail.com>
2023-06-10 18:36:16 +0700

    Initial commit

---
+++ feature/chat.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class Chat():
+    def __init__(self) -> None:
+        pass
---
+++ main.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from feature import chat
+
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
+
+class ExamplePage(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 1")
+       label.pack(side="top", fill="both", expand=True)
+
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        ex = ExamplePage(self)
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        ex.place(in_=container, x=0, y=0, relwidth=1, relheight=1)     
+
+        ex.show()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("1000x800")
+    root.mainloop()
